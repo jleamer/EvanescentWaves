27dc085edba04c3384462880de1d3cfb460fa5c3
jleamer <jleamer@tulane.edu>
2021-06-10 17:42:53 -0500

    Switched to new simulation data for a gap thickness between prisms ranging from wavelength/2 to 3*wavelength/2.  Additionally implemented method for calculating power across boundaries and code to plot variation in transmitted power vs alpha parameter that tunes the DOP of initial light

--- plot_results.py
+++ plot_results.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.interpolate import LinearNDInterpolator
 from scipy.interpolate import interp1d
+import scipy.integrate as integrate
 import numpy as np
 import os
 
@@ -173,6 +174,48 @@ def compare_pols(pol1, pol2, save, tol=1e-4):
     return
 
 
+def incident_edge(filename, tol=1e-4, numpoints=600):
+    """
+    Function for obtaining field values on the incident boundary from COMSOL data files
+    :param filename:    name of data file to get
+    :param tol:         tolerance in x value for getting field values on boundary
+    :param numpoints:   number of points to use in interpolation
+    :return:            y values and polarization vector for field
+    """
+    # Get rows that are on boundary
+    df = pd.read_csv(filename, sep=',', header=0)
+    min_X = df['X'].to_numpy().min()
+    rows = df.query('X-@min_X <= @tol & -(@min_X-X) <= @ @tol').index.to_numpy()
+
+    # Read out relevant data
+    y = np.zeros(rows.size, dtype=complex)
+    Ey = np.zeros(rows.size, dtype=complex)
+    Ez = np.zeros(rows.size, dtype=complex)
+
+    for n in range(rows.size):
+        y[n] = df.at[rows[n], 'Y']
+        Ey[n] = df.at[rows[n], 'Ey']
+        Ez[n] = df.at[rows[n], 'Ez']
+
+    # Interpolate Ey, Ez so that the final array has size numpoints
+    fy = interp1d(y, Ey, kind='linear')
+    fz = interp1d(y, Ez, kind='linear')
+
+    # Create new array of y values and use interpolation functions
+    ymin = y.min()  # df['Y'].to_numpy().min()
+    ymax = y.max()  # df['Y'].to_numpy().max()
+    iny = np.linspace(ymin, ymax, numpoints)
+    inEy = fy(iny)
+    inEz = fz(iny)
+
+    E = np.zeros((numpoints, 2), dtype=complex)
+    for n in range(numpoints):
+        E[n][0] = inEy[n]
+        E[n][1] = inEz[n]
+
+    return iny, E
+
+
 def transmitted_edge(filename, tol=1e-4, numpoints=600):
     """
     Function for obtaining field values on the transmitted boundary from COMSOL data files
@@ -186,7 +229,7 @@ def transmitted_edge(filename, tol=1e-4, numpoints=600):
     max_X = df['X'].to_numpy().max()
     rows = df.query('X-@max_X <= @tol & @max_X-X <= @ @tol').index.to_numpy()
 
-    # Read out relevant data
+    #Read out relevant data
     y = np.zeros(rows.size, dtype=complex)
     Ey = np.zeros(rows.size, dtype=complex)
     Ez = np.zeros(rows.size, dtype=complex)
@@ -201,8 +244,8 @@ def transmitted_edge(filename, tol=1e-4, numpoints=600):
     fz = interp1d(y, Ez)
 
     # Create new array of y values and use interpolation functions
-    ymin = df['Y'].to_numpy().min()
-    ymax = df['Y'].to_numpy().max()
+    ymin = 0  #df['Y'].to_numpy().min()
+    ymax = 10 #df['Y'].to_numpy().max()
     iny = np.linspace(ymin, ymax, numpoints)
     inEy = fy(iny)
     inEz = fz(iny)
@@ -213,6 +256,50 @@ def transmitted_edge(filename, tol=1e-4, numpoints=600):
         E[n][1] = inEz[n]
 
     return iny, E
+
+
+def reflected_edge(filename, tol=1e-4, numpoints=600):
+    """
+    function for obtaining the field values from the COMSOL simulation along reflected edge
+    :param filename:    name of the file data is stored in
+    :param tol:         tolerance to use when grabbing the appropriate rows
+    :param numpoints:   number of points to use in interpolation
+    :return:            array of x values and vector field
+    """
+    # Get rows that are on the boundary
+    df = pd.read_csv(filename, sep=',', header=0)
+    max_Y = df['Y'].to_numpy().max()
+    rows = df.query("Y-@max_Y <= @tol & @max_Y-Y <= @tol").index.to_numpy()
+
+    # Arrays for storing data
+    x = np.zeros(rows.size, dtype=complex)
+    Ex = np.zeros(rows.size, dtype=complex)
+    Ez = np.zeros(rows.size, dtype=complex)
+
+    # Store data
+    for i in range(rows.size):
+        x[i] = df.at[rows[i], 'X']
+        Ex[i] = df.at[rows[i], 'Ex']
+        Ez[i] = df.at[rows[i], 'Ez']
+
+    # Interpolate to make sure data are the right-size
+    fx = interp1d(x, Ex)
+    fz = interp1d(x, Ez)
+
+    # Create new arrays using interpolation functions
+    xmin = 0
+    xmax = 10
+    x = np.linspace(xmin, xmax, numpoints)
+    inEx = fx(x)
+    inEz = fz(x)
+
+    # Construct vector to return
+    E = np.zeros((numpoints, 2), dtype=complex)
+    for i in range(numpoints):
+        E[i][0] = inEx[i]
+        E[i][1] = inEz[i]
+
+    return x, E
 
 
 def get_J(yE, zE, alpha, beta, numpoints=600):
@@ -231,36 +318,101 @@ def get_J(yE, zE, alpha, beta, numpoints=600):
     return j
 
 
-def get_DOP(j, numpoints=600):
+def get_DOP(j, x, numpoints=600):
     """
     function to get DOP from coherency matrices in j
     :param j:           array of coherency matrices
+    :param x:           the position values
     :param numpoints:   number of points to use in array for DOP
     :return:            array of DOP values
     """
-    dop = np.zeros(numpoints, dtype=complex)
-    for n in range(numpoints):
-        s0 = j[n][0][0] + j[n][1][1]
-        s1 = j[n][0][0] - j[n][1][1]
-        s2 = j[n][0][1] + j[n][1][0]
-        s3 = -1j*(j[n][0][1] - j[n][1][0])
-        dop[n] = np.sqrt(s1**2 + s2**2 + s3**2)/s0
+    s0 = j[:, 0, 0] + j[:, 1, 1]
+    s1 = j[:, 0, 0] - j[:, 1, 1]
+    s2 = j[:, 0, 1] + j[:, 1, 0]
+    s3 = -1j*(j[:, 0, 1] - j[:, 1, 0])
+    dop = np.sqrt(s1**2 + s2**2 + s3**2)/s0
+    power = integrate.trapezoid(s0, x)
+    return dop, s0, power
 
-    return dop
+
+def get_Pow(j, x, numpoints=600):
+    """
+    function for obtaining the power across the boundary j is obtained over
+    :param j:           the coherency matrix
+    :param x:           the position array the coherency matrix is defined over
+    :param numpoints:   the number of points in j
+    :return:
+    """
+    # First get the intensity at each point: s0
+    s0 = j[:, 0, 0] + j[:, 1, 1]
+
+    # Now integrate
+    power = integrate.trapezoid(s0, x)
+    return power
+
 
 ypath = "YPol"
 zpath = "ZPol"
 
-test = "YPol/ypol_thickness_1.1E-7.csv"
-test2 = "ZPol/zpol_thickness_1.1E-7.csv"
-y, yE = transmitted_edge(test)
-y, zE = transmitted_edge(test2)
-alpha = 1/np.sqrt(2)
-beta = 1/np.sqrt(2)
-j = get_J(yE, zE, alpha, beta)
-dop = get_DOP(j)
+# Get filenames
+yfiles = []
+zfiles = []
+for root, dirs, files in os.walk(ypath):
+    for name in files:
+        yfiles.append(os.path.join(root, name))
 
-plt.plot(y, dop)
-plt.ylabel("DOP")
-plt.xlabel("y(um)")
+for root, dirs, files in os.walk(zpath):
+    for name in files:
+        zfiles.append(os.path.join(root, name))
+
+# Sort files
+yfiles.sort()
+zfiles.sort()
+print(yfiles)
+print(zfiles)
+# Check effect of changing alpha on power transmitted and reflected
+alpha = np.linspace(0, 1, 10)
+i_power = np.zeros(10, dtype=complex)
+t_power = np.zeros(10, dtype=complex)
+r_power = np.zeros(10, dtype=complex)
+n = 3
+iy, yiE = incident_edge(yfiles[n])
+iy, ziE = incident_edge(zfiles[n])
+y, ytE = transmitted_edge(yfiles[n])
+y, ztE = transmitted_edge(zfiles[n])
+x, xrE = reflected_edge(yfiles[n])
+x, zrE = reflected_edge(zfiles[n])
+
+plt.figure(1)
+plt.title("Power vs alpha for t=" + yfiles[n][20:25])
+plt.xlabel("Alpha^2")
+plt.ylabel("Power")
+for i in range(alpha.size):
+    i_j = get_J(yiE, ziE, alpha[i], np.sqrt(1 - alpha[i] ** 2))
+    trans_j = get_J(ytE, ztE, alpha[i], np.sqrt(1 - alpha[i] ** 2))
+    refl_j = get_J(xrE, zrE, alpha[i], np.sqrt(1 - alpha[i] ** 2))
+    dop, s0, i_power[i] = get_DOP(i_j, iy)
+    t_power[i] = get_DOP(trans_j, y)[2]
+    r_power[i] = get_DOP(refl_j, x)[2]
+plt.plot(alpha**2, i_power, 's-', label="Incident")
+plt.plot(alpha**2, t_power, label="Transmitted")
+plt.plot(alpha**2, r_power, label="Reflected")
+plt.plot(alpha**2, t_power+r_power, 'x-', label="T+R")
+plt.legend(numpoints=1)
+
+plt.figure(2)
+plt.title("Transmitted Power vs. alpha")
+plt.xlabel("Alpha^2")
+plt.ylabel("Power")
+power = np.zeros(10, dtype=complex)
+
+for i in range(len(yfiles)):
+    label = "t=" + yfiles[i][20:25]
+    y, yE = transmitted_edge(yfiles[i])
+    y, zE = transmitted_edge(zfiles[i])
+    for k in range(alpha.size):
+        j = get_J(yE, zE, alpha[k], np.sqrt(1 - alpha[k] ** 2))
+        power[k] = get_DOP(j, y)[2]
+    plt.plot(alpha**2, power, label=label)
+plt.legend(numpoints=1)
 plt.show()
